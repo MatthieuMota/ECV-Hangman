e3f373014bb6b2df70f958da0eea435c8ab0b8d1
Matthieu Mota <matthieumota@gmail.com>
2018-05-02 15:12:49 +0200

    Base layout

---
+++ src/AppBundle/Controller/GameController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the projet-avec-composer package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GameController extends Controller
+{
+    /**
+     * @Route("/game")
+     */
+    public function indexAction()
+    {
+        return $this->render('game/index.html.twig');
+    }
+}
